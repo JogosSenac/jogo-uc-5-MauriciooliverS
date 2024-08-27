dd4310a53dd4807601499a88e4dcc0e1a72d74d9
Mauricio de Oliveira e Silva <Mauricio50500546@edu.df.senac.br>
2024-08-27 09:49:38 -0300

    Atulização 27.08

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public bool comVida = true;
+    public float moveH;
+    public int velocidade;
+    public int forcaPulo;
+    private Rigidbody2D rb;
+    private Animator animPlayer;
+    public bool isJumping = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animPlayer = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -15,6 +26,39 @@ public class playerMovement : MonoBehaviour
     {
         //Andar
         float moveH = Input.GetAxis("Horizontal");
-        transform.position += new vector3(moveH * velocidade * Time.deltaTime, 0, 0);
+        transform.position += new Vector3(moveH * velocidade * Time.deltaTime, 0, 0);
+        if(Input.GetKey(KeyCode.W))
+        {
+            animPlayer.SetLayerWeight(2,1);
+        }
+        else
+        {
+            animPlayer.SetLayerWeight(2,0);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            animPlayer.SetLayerWeight(1,1);
+        }
+        else
+        {
+            animPlayer.SetLayerWeight(1,0);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            rb.AddForce(transform.up * forcaPulo,ForceMode2D.Impulse);
+        }
+        } 
+    private void OnCollisionEnter2D(Collision2D other)
+        {
+            if(other.gameObject.CompareTag("Chão"))
+            {
+                isJumping = false;
+            }         
+            if(other.gameObject.CompareTag("Morte"))
+            {
+                comVida = false;
+                Destroy(other.gameObject);
+            }
+        }
     }
-}
